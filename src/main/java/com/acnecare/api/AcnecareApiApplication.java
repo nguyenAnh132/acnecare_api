61dfddac9af86a521ba9724a499fe8f3fdff6c7c
NguyenAnh <nguyenanhkg123@gmail.com>
2026-02-28 16:09:59 +0700

    Initial commit

---
+++ src/main/java/com/acnecare/api/AcnecareApiApplication.java
@@ -0,0 +1,13 @@
+package com.acnecare.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcnecareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcnecareApiApplication.class, args);
+	}
+
+}
