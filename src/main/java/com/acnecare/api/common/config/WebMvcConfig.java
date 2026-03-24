894b4e617d4839d103c45bd8b9ee3511c8205cc7
tranbatong <tongxayda00@gmail.com>
2026-03-24 21:49:44 +0700

    feat: add realtime post

--- src/main/java/com/acnecare/api/common/config/WebMvcConfig.java
+++ src/main/java/com/acnecare/api/common/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.acnecare.api.common.config;
 import com.acnecare.api.common.storage.StorageProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // Map URL /files/** → thư mục ./uploads/ trên disk
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("file:" + storageProperties.getUploadDir() + "/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") 
+                .allowedOriginPatterns("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // Cờ này rất quan trọng đối với SockJS và WebSocket
     }
 }
