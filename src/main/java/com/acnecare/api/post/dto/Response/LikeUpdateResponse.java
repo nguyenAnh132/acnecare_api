cec28440db14c19d585dcc1ad544f2d54d66a4de
Nguyễn Văn Tú Anh <145957834+nguyenAnh132@users.noreply.github.com>
2026-04-01 14:49:17 +0700

    feat: post pageable

---
+++ src/main/java/com/acnecare/api/post/dto/Response/LikeUpdateResponse.java
@@ -0,0 +1,19 @@
+package com.acnecare.api.post.dto.Response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LikeUpdateResponse {
+    String postId;
+    boolean liked;
+    long likesCount;
+}
